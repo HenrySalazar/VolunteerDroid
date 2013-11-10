89efaad8ef7407e145570e2369866bc8354ae1bc
HenrySalazar <laughinggiraffe0@gmail.com>
2013-11-09 21:31:21 -0500

    added navigation drawer

--- VolunteerMe/gen/tut1/login/R.java
+++ VolunteerMe/gen/tut1/login/R.java
@@ -8,33 +8,45 @@
 package tut1.login;
 
 public final class R {
+    public static final class array {
+        public static final int drawer_strings=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class drawable {
-        public static final int glossyarmwrestler=0x7f020000;
-        public static final int logo=0x7f020001;
-        public static final int logo128=0x7f020002;
+        public static final int drawer_shadow=0x7f020000;
+        public static final int glossyarmwrestler=0x7f020001;
+        public static final int logo=0x7f020002;
+        public static final int logo128=0x7f020003;
     }
     public static final class id {
-        public static final int button1=0x7f060004;
-        public static final int editText1=0x7f060002;
-        public static final int editText2=0x7f060003;
-        public static final int logo128=0x7f060001;
-        public static final int showmsg=0x7f060005;
-        public static final int volunteerme=0x7f060000;
+        public static final int button1=0x7f070009;
+        public static final int content_frame=0x7f070001;
+        public static final int drawer_layout=0x7f070000;
+        public static final int editText1=0x7f070007;
+        public static final int editText2=0x7f070008;
+        public static final int eventsText=0x7f070004;
+        public static final int expandableListView=0x7f070003;
+        public static final int left_drawer=0x7f070002;
+        public static final int logo128=0x7f070006;
+        public static final int volunteerme=0x7f070005;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int second=0x7f030001;
+        public static final int drawer=0x7f030000;
+        public static final int drawer_list_item=0x7f030001;
+        public static final int expandable_list_view=0x7f030002;
+        public static final int fragment_events=0x7f030003;
+        public static final int fragment_find=0x7f030004;
+        public static final int login=0x7f030005;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int btn_txt=0x7f040004;
-        public static final int hello=0x7f040000;
-        public static final int password=0x7f040003;
-        public static final int username=0x7f040002;
+        public static final int app_name=0x7f050001;
+        public static final int btn_txt=0x7f050004;
+        public static final int hello=0x7f050000;
+        public static final int password=0x7f050003;
+        public static final int username=0x7f050002;
     }
     public static final class style {
-        public static final int NoActionBar=0x7f050000;
+        public static final int NoActionBar=0x7f060000;
     }
 }
---
+++ VolunteerMe/src/tut1/login/EventData.java
@@ -0,0 +1,17 @@
+package tut1.login;
+
+import java.util.Date;
+
+public class EventData {
+	
+	public static final int childDataTotal = 3; 
+	
+    public String eventName;
+    public String description;
+    public String location;
+    public Date beginTime;
+    public Date endTime;
+
+    public EventData() {
+    }
+}
